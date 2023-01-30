ebba03d18eaddfbe620aa4bea5adc21a15c2708a
qtaizao <123918119+qtaizao@users.noreply.github.com>
2023-01-30 16:02:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,20 +23,20 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oONpg6bWmUl-aN03LZsFvetNGhkY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '6cMbaQod0lBcbfaTrQWEl_R_1Z_HNIqSvuf80t4c0xw',
+      useTemplateId: '7jKxdDE4xV5I4NEPY5hQp-XfjJqs28vXy9wg-fiJXuw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-30',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
+       // {
           type: '*生日', name: '宝贝', year: '2000', date: '05-30',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
+        //{
           type: '生日', name: '宝贝', year: '2000', date: '04-27',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2022', date: '09-08',
+          type: '节日', name: '认识', year: '2022', date: '09-08',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
