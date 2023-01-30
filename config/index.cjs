65005ad893973bcabbf0e37cb352b8fb64953959
qtaizao <123918119+qtaizao@users.noreply.github.com>
2023-01-30 14:21:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oONpg6bWmUl-aN03LZsFvetNGhkY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'I5SO1uzbBpyrgKRu8fwWVQ3YIp8dF8qu5JQGqwflbaw',
+      useTemplateId: 'YiRYqLCxuX6hh5P1YZn8v5Etsewyfftgmk8RaD5quTQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-30',
       festivals: [
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'I5SO1uzbBpyrgKRu8fwWVQ3YIp8dF8qu5JQGqwflbaw',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oONpg6bWmUl-aN03LZsFvetNGhkY',
+      id: '',
     }
   ],
 
