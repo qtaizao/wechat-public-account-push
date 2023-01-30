993fa9d04f2f4e5bff748de9c8404b81f5629aee
qtaizao <123918119+qtaizao@users.noreply.github.com>
2023-01-30 16:57:54 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,9 +19,9 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '开新',
+      name: ['开新', '太早'],
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id:'oONpg6asbsqw7oeLwEdppYy1jU5E',
+      id:['oONpg6asbsqw7oeLwEdppYy1jU5E', 'oONpg6bWmUl-aN03LZsFvetNGhkY' ]
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'P40HrrBrYOv1uDiuVi4Z2gZ9pnbjjP_nhLGdRgyOp3c',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -54,7 +54,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'P40HrrBrYOv1uDiuVi4Z2gZ9pnbjjP_nhLGdRgyOp3c',
 
   CALLBACK_USERS: [
     {
