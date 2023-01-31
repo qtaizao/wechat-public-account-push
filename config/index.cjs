5b216c961155e44ffff33f2d876bd1ca7ab9ada4
qtaizao <123918119+qtaizao@users.noreply.github.com>
2023-01-31 09:32:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,11 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '开新',
+      name: '太早',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oONpg6bWmUl-aN03LZsFvetNGhkY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'HUKY6IIqYPNJv7ZbnAdLYF3ZFQV5wrd50HfKbAzErIY',
+      useTemplateId: 'HWv7x44EZINtUbvaI7wQTGmCR9ZOex9y9kxbOLXnMAU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-30',
       festivals: [
@@ -44,6 +44,8 @@ const USER_CONFIG = {
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
+        // 初识的日子
+        { keyword: 'acquaintance_day', date: '2022-09-08' },
         // 在一起的日子
         { keyword: 'love_day', date: '2022-10-29' },
         // 一周年纪念日
@@ -54,7 +56,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'P40HrrBrYOv1uDiuVi4Z2gZ9pnbjjP_nhLGdRgyOp3c',
+  CALLBACK_TEMPLATE_ID: 'HWv7x44EZINtUbvaI7wQTGmCR9ZOex9y9kxbOLXnMAU',
 
   CALLBACK_USERS: [
     {
